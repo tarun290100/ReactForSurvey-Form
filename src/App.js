a8da8ad0da0c8b10753082e225a785f01c917499
Tarunjai29 <bt19ece060@iiitn.ac.in>
2023-07-24 23:20:38 +0530

    react project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
+import Header from "./MyComponents/Header"
+import {Footer} from "./MyComponents/Footer"
+import {Todos} from "./MyComponents/Todos"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+    <Header  searchBar={false}/>
+    <Todos/>
+    <Footer/>
+
+    </>
+  
   );
 }
 
